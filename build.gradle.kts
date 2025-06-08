9a2bc845e8000c96e22088a6b3ee4f5050475bad
erichuang0324 <121286342+erichuang0324@users.noreply.github.com>
2025-06-08 19:24:10 +0800

    Utilization of javafx for the screen of the MLA citation machine

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.jsoup:jsoup:1.20.1")
+    implementation("org.openjfx:javafx-controls:24.0.1")
+    implementation("org.openjfx:javafx-fxml:24.0.1")
 }
 
 tasks.test {
